434fc6b02b019753715292f975a27676909aad90
utopiafallen@gmail.com <utopiafallen@gmail.com@c48847ec-dfe4-ff80-3ff3-ddcc0cef2624>
2012-05-07 20:23:25 +0000

    Updating pch includes.

--- source/subtitlecore/stdafx.h
+++ source/subtitlecore/stdafx.h
@@ -9,10 +9,23 @@
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 
-
 // Direct2D
 #include "D2D1.h"
 #include "D2D1Helper.h"
 
 // DirectWrite
 #include "DWrite.h"
+
+// Microsoft Concurrency Runtime
+#include <ppl.h>
+#include <concurrent_queue.h>
+#include <concurrent_vector.h>
+
+// STL
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <tuple>

60bce21e4ee0d511a99eb1eeb98e54f83922b3c4
utopiafallen@gmail.com <utopiafallen@gmail.com@c48847ec-dfe4-ff80-3ff3-ddcc0cef2624>
2012-05-14 22:28:18 +0000

    Lots of DirectShow work. Filter should now properly register/unregister itself. Implemented CheckInputType, GetMediaType, and SetMediaType.
    Still need to add in additional AM_MEDIATYPES to properly accept subtitle data.

--- source/dxsubfilter/dxsubfilter.cpp
+++ source/dxsubfilter/dxsubfilter.cpp
@@ -4,15 +4,7 @@
 #include "stdafx.h"
 #include "dxsubfilter.h"
 
-
-// This is an example of an exported variable
-DXSUBFILTER_API int ndxsubfilter=0;
-
-// This is an example of an exported function.
-DXSUBFILTER_API int fndxsubfilter(void)
-{
-	return 42;
-}
+using namespace DXSubFilter;
 
 // This is the constructor of a class that has been exported.
 // see dxsubfilter.h for the class definition
@@ -34,7 +26,37 @@ CUnknown* CDXSubFilter::CreateInstance(LPUNKNOWN pUnk, HRESULT* phr)
 
 HRESULT CDXSubFilter::CheckInputType(const CMediaType* mtIn)
 {
-	return S_OK;
+	// We only accept video and text
+	if (mtIn->majortype == MEDIATYPE_Video)
+	{
+		// Check to see if the video subtype is one of the supported subtypes
+		for (size_t i = 0; i < DXSUBFILTER_SUPPORTED_VIDEO_SUBTYPES_8BIT_COUNT; i++)
+		{
+			if (mtIn->subtype == DXSUBFILTER_SUPPORTED_VIDEO_SUBTYPES_8BIT[i])
+			{
+				return S_OK;
+			}
+		}
+
+		for (size_t i = 0; i < DXSUBFILTER_SUPPORTED_VIDEO_SUBTYPES_16BIT_COUNT; i++)
+		{
+			if (mtIn->subtype == DXSUBFILTER_SUPPORTED_VIDEO_SUBTYPES_16BIT[i])
+			{
+				return S_OK;
+			}
+		}
+
+		// Not a supported subtype so fail
+		return VFW_E_TYPE_NOT_ACCEPTED;
+	}
+	else if (mtIn->majortype == MEDIATYPE_Text)
+	{
+		return S_OK;
+	}
+	else
+	{
+		return VFW_E_TYPE_NOT_ACCEPTED;
+	}
 }
 
 HRESULT CDXSubFilter::CheckTransform(const CMediaType* mtIn, const CMediaType* mtOut)
@@ -49,7 +71,71 @@ HRESULT CDXSubFilter::DecideBufferSize(IMemAllocator * pAllocator, ALLOCATOR_PRO
 
 HRESULT CDXSubFilter::GetMediaType(int iPosition, CMediaType *pMediaType)
 {
-	return S_OK;
+	if (iPosition < 0)
+	{
+		return E_INVALIDARG;
+	}
+	else if (m_pInput->IsConnected() == TRUE)
+	{
+		// In the special case of 10/16-bit input, we will offer 8-bit formats as output so that
+		// we can force the video decoder to output in 8-bit if the video renderer can't accept 
+		// 10/16-bit input. Otherwise, we only offer the input media type as an output type.
+		bool bInputIs16Bit = false;
+		for (size_t i = 0; i < DXSUBFILTER_SUPPORTED_VIDEO_SUBTYPES_16BIT_COUNT; i++)
+		{
+			if (m_InputVideoType.subtype == DXSUBFILTER_SUPPORTED_VIDEO_SUBTYPES_16BIT[i])
+			{
+				bInputIs16Bit = true;
+				break;
+			}
+		}
+
+		if (bInputIs16Bit)
+		{
+			// Try input format first
+			if (iPosition == 0)
+			{
+				*pMediaType = m_InputVideoType;
+			}
+			else if (iPosition > DXSUBFILTER_SUPPORTED_VIDEO_SUBTYPES_8BIT_COUNT)
+			{
+				return VFW_S_NO_MORE_ITEMS;
+			}
+			else
+			{
+				// Subtract 1 from iPosition because iPosition == 0 is used for input format.
+				pMediaType->SetSubtype(&DXSUBFILTER_SUPPORTED_VIDEO_SUBTYPES_8BIT[iPosition-1]);
+			}
+		}
+		else
+		{
+			*pMediaType = m_InputVideoType;
+		}
+		return S_OK;
+	}
+	else
+	{
+		return VFW_S_NO_MORE_ITEMS;
+	}
+}
+
+HRESULT CDXSubFilter::SetMediaType(PIN_DIRECTION direction,const CMediaType *pmt)
+{
+	// Call base class SetMediaType first and see if everything's ok
+	HRESULT hr = CTransformFilter::SetMediaType(direction, pmt);
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+	else
+	{
+		// Save the input media type from video decoder
+		if (direction == PINDIR_INPUT && pmt->majortype == MEDIATYPE_Video)
+		{
+			m_InputVideoType = *pmt;
+		}
+		return S_OK;
+	}
 }
 
 HRESULT CDXSubFilter::Transform(IMediaSample * pIn, IMediaSample *pOut)
--- source/dxsubfilter/dxsubfilter.h
+++ source/dxsubfilter/dxsubfilter.h
@@ -15,44 +15,69 @@
 #include "stdafx.h"
 #include "dxsubfilter_uuids.h"
 
-// This class is exported from the dxsubfilter.dll
-//class DXSUBFILTER_API CDXSubFilter
-//{
-//public:
-//	CDXSubFilter(void);
-//	// TODO: add your methods here.
-//};
-
-class __declspec(uuid("3B6ED1B8-ECF6-422A-8F07-48980E6482CE")) CDXSubFilter : public CTransformFilter
+namespace DXSubFilter
 {
-public: // Functions
-	static CUnknown* WINAPI CreateInstance(LPUNKNOWN pUnk, HRESULT* phr);
+	static const GUID DXSUBFILTER_SUPPORTED_VIDEO_SUBTYPES_8BIT[] = {
+		MEDIASUBTYPE_AYUV,	// Packed 4:4:4
+		MEDIASUBTYPE_YUY2,	// Packed 4:2:2
+		MEDIASUBTYPE_YV12,	// Planar 4:2:0
+		MEDIASUBTYPE_NV12,	// Planar/Packed 4:2:0
+	};
+
+	static const GUID DXSUBFILTER_SUPPORTED_VIDEO_SUBTYPES_16BIT[] = {
+		MEDIASUBTYPE_P210,	// Planar 4:2:2
+		MEDIASUBTYPE_P216,	// Planar 4:2:2
+		MEDIASUBTYPE_P010,	// Planar 4:2:0
+		MEDIASUBTYPE_P016,	// Planar 4:2:0
+	};
+
+	static const size_t DXSUBFILTER_SUPPORTED_VIDEO_SUBTYPES_8BIT_COUNT = 
+						ARRAYSIZE(DXSUBFILTER_SUPPORTED_VIDEO_SUBTYPES_8BIT);
+
+	static const size_t DXSUBFILTER_SUPPORTED_VIDEO_SUBTYPES_16BIT_COUNT = 
+						ARRAYSIZE(DXSUBFILTER_SUPPORTED_VIDEO_SUBTYPES_16BIT);
+
+	class __declspec(uuid("3B6ED1B8-ECF6-422A-8F07-48980E6482CE")) CDXSubFilter : public CTransformFilter
+	{
+	public: // Functions
+		static CUnknown* WINAPI CreateInstance(LPUNKNOWN pUnk, HRESULT* phr);
 	
-	CDXSubFilter(LPUNKNOWN pUnk);
-	virtual ~CDXSubFilter();
+		CDXSubFilter(LPUNKNOWN pUnk);
+		virtual ~CDXSubFilter();
 
-	//===================================================
-	// CTransformFilter abstract overrides
-	//===================================================
+		//===================================================
+		// CTransformFilter overrides
+		//===================================================
 
-	// Perform transform
-	virtual HRESULT Transform(IMediaSample * pIn, IMediaSample *pOut);
+		// Perform transform
+		virtual HRESULT Transform(IMediaSample * pIn, IMediaSample *pOut);
 
-    // Check if we can support mtIn
-    virtual HRESULT CheckInputType(const CMediaType* mtIn);
+		// Check if we can support mtIn
+		virtual HRESULT CheckInputType(const CMediaType* mtIn);
 
-    // Check if we can support the transform from this input to this output
-    virtual HRESULT CheckTransform(const CMediaType* mtIn, const CMediaType* mtOut);
+		// Check if we can support the transform from this input to this output
+		virtual HRESULT CheckTransform(const CMediaType* mtIn, const CMediaType* mtOut);
 
-    // Call the SetProperties function with appropriate arguments
-    virtual HRESULT DecideBufferSize(
-                        IMemAllocator * pAllocator,
-                        ALLOCATOR_PROPERTIES *pprop);
+		// Call the SetProperties function with appropriate arguments
+		virtual HRESULT DecideBufferSize(
+							IMemAllocator * pAllocator,
+							ALLOCATOR_PROPERTIES *pprop);
 
-    // Suggest OUTPUT pin media types
-    virtual HRESULT GetMediaType(int iPosition, CMediaType *pMediaType);
+		// Suggest OUTPUT pin media types
+		virtual HRESULT GetMediaType(int iPosition, CMediaType *pMediaType);
+		
+		// Called when upstream filter decides what media type to pass into us
+		virtual HRESULT SetMediaType(PIN_DIRECTION direction,const CMediaType *pmt);
+
+	protected:
+		// We use m_pInput inherited from CTransformFilter for our video input so we use a second
+		// pin to accept text subtitle data.
+		CTransformInputPin* m_pInputSubtitle;
+
+		// Save the video format that upstream decided on so we can pass the same format downstream.
+		// We will not perform any video format conversions. However, in the special case of 
+		// 10/16-bit input, we will offer 8-bit formats as output so that we can force the video
+		// decoder to output in 8-bit if the video renderer can't accept 10/16-bit input.
+		CMediaType m_InputVideoType;
+	};
 };
-
-extern DXSUBFILTER_API int ndxsubfilter;
-
-DXSUBFILTER_API int fndxsubfilter(void);

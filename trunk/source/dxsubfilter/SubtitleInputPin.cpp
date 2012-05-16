252f1c09f6e0d5ecc67548af8fc469d3992814b1
utopiafallen@gmail.com <utopiafallen@gmail.com@c48847ec-dfe4-ff80-3ff3-ddcc0cef2624>
2012-05-16 21:36:15 +0000

    Just realized the project will contain references to missing files, so here they are. Started work on custom SubtitleInputPin to handle subtitle data.

---
+++ trunk/source/dxsubfilter/SubtitleInputPin.cpp
@@ -0,0 +1,55 @@
+#include "stdafx.h"
+#include "dxsubfilter_uuids.h"
+#include "SubtitleInputPin.h"
+
+using namespace DXSubFilter;
+
+CSubtitleInputPin::CSubtitleInputPin(LPCWSTR pObjectName, CTransformFilter *pTransformFilter,
+	HRESULT * phr, LPCWSTR pName) : CTransformInputPin(pObjectName, pTransformFilter, phr, pName)
+{
+
+}
+
+CSubtitleInputPin::~CSubtitleInputPin()
+{
+
+}
+
+HRESULT CSubtitleInputPin::CheckMediaType(const CMediaType* mtIn)
+{
+	// Check the input type. We reject if it's anything other than subtitle data
+    HRESULT hr = S_OK;
+
+	if (mtIn->majortype != MEDIATYPE_Subtitle)
+	{
+		hr = S_FALSE;
+	}
+
+    return hr;
+}
+
+STDMETHODIMP CSubtitleInputPin::Receive(IMediaSample* pSample)
+{
+	HRESULT hr;
+	BYTE* pBufferIn;
+	REFERENCE_TIME sampleStartTime, sampleEndTime;
+	long lBufferLength;
+
+	hr = pSample->GetMediaTime(&sampleStartTime, &sampleEndTime);
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+	hr = pSample->GetPointer(&pBufferIn);
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+	lBufferLength = pSample->GetActualDataLength();
+
+	// The sample should just be a single line of subtitle data
+	std::string s(reinterpret_cast<char*>(pBufferIn));
+	return S_OK;
+}

71d52ad1451750763f692959e492ae932db5d40d
utopiafallen@gmail.com <utopiafallen@gmail.com@c48847ec-dfe4-ff80-3ff3-ddcc0cef2624>
2012-05-24 17:43:07 +0000

    Updated ISubtitleRenderer with Invalidate function.

--- source/subtitlecore/SRTSubtitleRenderer.h
+++ source/subtitlecore/SRTSubtitleRenderer.h
@@ -19,11 +19,19 @@ namespace SubtitleCore
 		//==================================================================
 		virtual bool ParseScript(const std::wstring& script);
 		virtual bool ParseLine(const std::wstring& line);
+
+		// The following functions do nothing in this implementation as SRT does not have a 
+		// script header
 		virtual bool ParseFormatHeader(const std::wstring& header);
 		virtual bool ParseFormatHeader(const unsigned char* header);
+
+		// SRT is text based so no binary processing needed
 		virtual bool ParseData(const unsigned char* data);
 		virtual bool ParseData(const unsigned char* data, ptrdiff_t offset);
 		virtual bool ParseData(const unsigned char* data, ptrdiff_t startOffset, ptrdiff_t endOffset);
+
+		virtual void Invalidate();
+
 		virtual size_t GetSubtitlePictureCount(REFERENCE_TIME rtNow);
 		virtual void GetSubtitlePicture(REFERENCE_TIME rtNow, SubtitlePicture** ppOutSubPics);
 
